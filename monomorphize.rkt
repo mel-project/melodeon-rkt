40b00140995375c2a49a68076c08aa8a9489e11c
Jay Butera <buterajay@protonmail.com>
2021-07-14 17:18:49 -0500

    save progress on generic->conrete fn

--- monomorphize.rkt
+++ monomorphize.rkt
@@ -36,7 +36,7 @@
 ; and returns a @def-fun definition where the generic types are replaced
 ; with types inferred by the application; or nothing if inference is not
 ; possible.
-(: generic->concrete (-> Definition @-Ast (Option Definition)))
+(: generic->concrete (-> Definition @-Ast Definition))
 (define (generic->concrete gen-def app)
   (cond
     [(match gen-def [`(@def-generic-fun _ _ _ _ _) #t]
@@ -72,4 +72,29 @@
                            (type-contains t (sym->type-var generic-sym))))
                        (zip (range (length param-types)) param-types)))
              generic-syms))])
-        #f)]))
+        ; Produce a concrete fn definition from argument types
+        (match gen-def
+          [`(@def-generic-fun ,name _ ,binds ,return-type ,body)
+            (let
+              [concrete-binds
+                (map (lambda ([enumerated-bind : (List Index (List Symbol Type-Expr))])
+                       (let [i (car enumerated-bind)]
+                         ; Return a new binding with same name
+                         (list (cdar enumerated-bind)
+                               ; if this parameter is generic, replace with the
+                               ; argument type, otherwise use parameter
+                               ; definition type.
+                               (if (member i param-idxs)
+                                   (list-ref arg-types i)
+                                   (cddar enumerated-bind)))))
+                     (zip (range (length binds)) binds))]
+              ; TODO implement
+              [concrete-return-type #f]
+              ; TODO check the body for applications of @def-generic-fun,
+              ; recursively resolve
+              [concrete-body body]
+            `(@def-fun
+               ,name
+               ,concrete-binds
+               ,concrete-return-type,
+               ,concrete-body)])))]))

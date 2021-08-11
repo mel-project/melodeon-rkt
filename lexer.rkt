8976f6b0618b6e6bc00aaf3ec1bc716f4fcde6d9
Jay Butera <buterajay@protonmail.com>
2021-08-11 13:57:24 -0500

    Single line comments

--- lexer.rkt
+++ lexer.rkt
@@ -95,6 +95,8 @@
    ["&" 'TAND]
    ["~" 'TNEG]
    [(:: "-" (:* #\space) "-" (:* #\space) "-") '---]
+   ;; skip comments
+   [(:: "--" any-string #\newline) (return-without-pos (melo-lex-once input-port))]
    ;; skip all whitespace
    [(:+ (:or #\tab #\space #\newline)) (return-without-pos (melo-lex-once input-port))]
    ;; pass-through arithmetic operations
